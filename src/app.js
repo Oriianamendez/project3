0145ceda41100e0fa7d5f8626d51ff20941bcbdd
Oriianamendez <egleemendez@gmail.com>
2023-09-21 13:47:16 +0200

    Search engine is working

--- src/app.js
+++ src/app.js
@@ -48,8 +48,20 @@ function displayTemperature(response) {
   currentIcon.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "6efaaef1baet493f7b254do70ae07eb3";
-let city = "Caracas";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit={metric}`;
+function search(city) {
+  let apiKey = "6efaaef1baet493f7b254do70ae07eb3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit={metric}`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let currentCityInput = document.querySelector("#city-input");
+  search(currentCityInput.value);
+}
+
+search("Caracas");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
